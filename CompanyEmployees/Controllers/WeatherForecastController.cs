cbae96af80f2110703f2b9a75881ae8a2ddbfcd5
Eric Tang <erictanggm@gmail.com>
2020-06-12 00:26:04 +0800

    2020-06-11

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private ILoggerManager _logger;
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public WeatherForecastController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInfo("info log message");
+            _logger.LogDebug("debug log message");
+            _logger.LogWarn("warn log message");
+            _logger.LogError("error log message");
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Entities/RepositoryContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Entities
+{
+    public class RepositoryContext
+    {
+        public RepositoryContext()
+        {
+        }
+    }
+}
